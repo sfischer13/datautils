cf40b2e83b05ee35d0718796982be5f3b849861e
Stefan Fischer <sfischer13@ymail.com>
2017-12-18 02:02:27 +0100

    Add interval.go

---
+++ interval.go
@@ -0,0 +1,86 @@
+package datautils
+
+import "math"
+import "regexp"
+import "strconv"
+import "strings"
+
+var reRanges = regexp.MustCompile(`` +
+	`^([0-9]*|[0-9]*:[0-9]*|[0-9]*:[0-9]*:[0-9]*)` +
+	`(,([0-9]*|[0-9]*:[0-9]*|[0-9]*:[0-9]*:[0-9]*))*$`)
+var reRangeS = regexp.MustCompile(`^(\d*)$`)
+var reRangeM = regexp.MustCompile(`^(\d*):(\d*)$`)
+var reRangeL = regexp.MustCompile(`^(\d*):(\d*):(\d*)$`)
+
+type Interval struct {
+	Low  int64
+	High int64
+	Step int64
+}
+
+func NewInterval(low, high, step string) Interval {
+	if low == "" {
+		low = "1"
+	}
+	if high == "" {
+		high = strconv.FormatInt(math.MaxInt64, 10)
+	}
+	if step == "" {
+		step = "1"
+	}
+
+	l, errL := strconv.ParseInt(low, 10, 64)
+	h, errH := strconv.ParseInt(high, 10, 64)
+	s, errS := strconv.ParseInt(step, 10, 64)
+
+	if (errL != nil) || (errH != nil) || (errS != nil) {
+		panic("Parameters could not be parsed!")
+	}
+
+	return Interval{
+		Low:  l,
+		High: h,
+		Step: s,
+	}
+}
+
+func (r *Interval) Contains(i int64) bool {
+	return (i >= r.Low) && (i <= r.High) && (((i - r.Low) % r.Step) == 0)
+}
+
+func String2Intervals(s string) []Interval {
+	if reRanges.MatchString(s) {
+		result := []Interval{}
+		for _, r := range strings.Split(s, ",") {
+			low := ""
+			high := ""
+			step := ""
+			if m := reRangeS.FindStringSubmatch(r); len(m) != 0 {
+				low = m[1]
+				high = m[1]
+			} else if m := reRangeM.FindStringSubmatch(r); len(m) != 0 {
+				low = m[1]
+				high = m[2]
+			} else if m := reRangeL.FindStringSubmatch(r); len(m) != 0 {
+				low = m[1]
+				high = m[2]
+				step = m[3]
+			} else {
+				return nil
+			}
+			result = append(result, NewInterval(low, high, step))
+		}
+		return result
+	}
+	return nil
+}
+
+func Intervals2Func(rs []Interval) func(int64) bool {
+	return func(i int64) bool {
+		result := false
+		for _, r := range rs {
+			result = result || r.Contains(i)
+		}
+		return result
+	}
+}
