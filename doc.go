c1e09abaf64e2ad8a3e1b5c12650dae722284e9e
Stefan Fischer <sfischer13@ymail.com>
2017-12-18 20:56:45 +0100

    Add doc.go

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Copyright (c) 2017 Stefan Fischer
+// Use of this code is governed by the MIT license, which can be found in the LICENSE file.
+
+// Package datautils is a collection of handy text manipulation tools.
+package datautils
